c4c4bb1ebda3beb8774e64947161b99083de0634
kidvip <15910676714@163.com>
2019-11-11 11:28:19 +0800

    'firstcommit'

---
+++ src/main/java/com/boco/springboot_kidvip/SpringbootKidvipApplication.java
@@ -0,0 +1,13 @@
+package com.boco.springboot_kidvip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKidvipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKidvipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boco/springboot_kidvip/SpringbootKidvipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boco.springboot_kidvip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKidvipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
